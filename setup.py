73333cb1a920dfbd8f2ce1fbe12945d4f9b7122d
mohaned <mohan3d94@gmail.com>
2016-07-21 22:59:53 +0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pyopenload',
+    version='0.1dev',
+    packages=['src'],
+    url='https://github.com/mohan3d/PyOpenload',
+    license='MIT',
+    author='Mohaned Magdy',
+    author_email='mohan3d94@gmail.com',
+    description='Just python wrapper for openload.co API',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'requests',
+    ],
+)
