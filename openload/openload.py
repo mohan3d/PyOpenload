42586e85ba7d2616640dab4058c2c1fb687248ea
mohaned <mohan3d94@gmail.com>
2016-08-12 14:58:11 +0200

    Add simple documentation for some methods

--- openload/openload.py
+++ openload/openload.py
@@ -6,11 +6,23 @@ class OpenLoad(object):
     api_version = '1'
 
     def __init__(self, api_login, api_key):
+        """Initializes OpenLoad instance with given parameters and formats api base url.
+
+        Args:
+            api_login (str): API Login found in openload.co
+            api_key (str): API Key found in openload.co
+
+        Returns:
+            None
+        """
         self.login = api_login
         self.key = api_key
         self.api_url = self.api_base_url.format(api_version=self.api_version)
-    
+
     def __get(self, url, params=None, logged_in=True, result_only=True):
+        """
+
+        """
         if not params:
             params = {}
 
@@ -20,14 +32,50 @@ class OpenLoad(object):
         response_json = requests.get(self.api_url + url, params).json()
 
         return response_json['result'] if result_only else response_json
-    
+
     def account_info(self, result_only=True):
+        """Requests account info of the user.
+
+        Args:
+            result_only (bool): if it is true,
+            only results are returned otherwise the whole response is returned.
+
+        Returns:
+            dict: dictionary containing response data of account_info request.
+        """
         return self.__get('account/info', result_only=result_only)
 
     def prepare_download(self, file_id, result_only=True):
+        """Makes a request to prepare for file download,
+        this download preparation will be used before get_download_link method.
+
+        Args:
+            file_id (str): id of the file to be downloaded,
+            say we have this url "https://openload.co/f/TJNMUk2hnYs/filename", TJNMUk2hnYs is the id of this file.
+
+            result_only (bool): if it is true, only results are returned otherwise the whole response is returned.
+
+        Returns:
+            dict: dictionary containing response of prepare_download request.
+        """
         return self.__get('file/dlticket', params={'file': file_id}, result_only=result_only)
 
     def get_download_link(self, file_id, ticket, captcha_response=None):
+        """Requests direct download link for requested file,
+        this method makes use of the response of prepare_download, prepare_download must be called first.
+
+        Args:
+            file_id (str): id of the file to be downloaded.
+
+            ticket (str): preparation ticket is found in prepare_download response,
+            this is why we need to call prepare_download before get_download_link.
+
+            captcha_response (str): sometimes prepare_download will have captcha url to be solved first,
+            this is the solution of the captcha.
+
+        Returns:
+            str: direct download link for the requested file.
+        """
         params = {'ticket': ticket, 'file': file_id}
 
         if captcha_response:
@@ -36,13 +84,47 @@ class OpenLoad(object):
         return self.__get('file/dl', params)
 
     def file_info(self, file_id, result_only=True):
+        """Used to request info for a specific file, info like size, name, .....
+
+        Args:
+            file_id (str): id of the file to be downloaded.
+            result_only (bool): if it is true, only results are returned otherwise the whole response is returned.
+
+        Returns:
+            dict: dictionary containing response of file_info request.
+        """
         return self.__get('file/info', params={'file': file_id}, result_only=result_only)
 
     def upload_link(self, result_only=True, **kwargs):
+        """Makes a request to prepare for file upload.
+
+        Args:
+            result_only (bool): if it is true, only results are returned otherwise the whole response is returned.
+
+            **kwargs: kwargs may contain (folder: Folder-ID to upload to,
+                sha1: Expected sha1 If sha1 of uploaded file doesn't match this value upload fails,
+                httponly: If this is set to true, use only http upload links).
+
+        Returns:
+            dict: dictionary containing response of upload_link request.
+        """
         params = {key: value for key, value in kwargs.items() if value}
         return self.__get('file/ul', params=params, result_only=result_only)
 
     def upload_file(self, file_path, **kwargs):
+        """Calls upload_link request to get valid url, then it makes a post request with given file to be uploaded.
+        No need to call upload_link explicitly since upload_file calls it.
+
+        Args:
+            file_path (str): full path of the file to be uploaded.
+
+            **kwargs: kwargs may contain (folder: Folder-ID to upload to,
+                sha1: Expected sha1 If sha1 of uploaded file doesn't match this value upload fails,
+                httponly: If this is set to true, use only http upload links).
+
+        Returns:
+            dict: dictionary containing response of upload_file request.
+        """
         response = self.upload_link(**kwargs)
         upload_url = response['url']
 
