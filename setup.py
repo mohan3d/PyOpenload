a2f377f38037e71c55dbfcc69f0df9cd32acaac7
mohaned <mohan3d94@gmail.com>
2016-08-19 19:37:27 +0200

    Add download_url in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='Mohaned Magdy',
     author_email='mohan3d94@gmail.com',
     url = 'https://github.com/mohan3d/PyOpenload',
+    download_url = 'https://github.com/mohan3d/PyOpenload/tarball/0.1',
     install_requires=[
         'requests',
     ],
