577af16a619cff31776b9616f1c2c5a550f79cca
mohaned <mohan3d94@gmail.com>
2016-08-19 20:35:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='pyopenload',
-    version='0.2',
+    version='0.3',
     packages=['openload'],
     url='https://github.com/mohan3d/PyOpenload',
     license='MIT',
     author='Mohaned Magdy',
     author_email='mohan3d94@gmail.com',
-    download_url = 'https://github.com/mohan3d/PyOpenload/tarball/0.2',
+    download_url = 'https://github.com/mohan3d/PyOpenload/tarball/0.3',
     install_requires=[
         'requests',
     ],
