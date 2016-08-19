e84778845acef12e15ac31260f37efeba32835d3
mohaned <mohan3d94@gmail.com>
2016-08-19 20:01:29 +0200

    Update setup only to fix problem with pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from distutils.core import setup
 
 setup(
     name='pyopenload',
-    version='0.1',
+    version='0.2',
     packages=['openload'],
     url='https://github.com/mohan3d/PyOpenload',
     license='MIT',
     author='Mohaned Magdy',
     author_email='mohan3d94@gmail.com',
-    download_url = 'https://github.com/mohan3d/PyOpenload/tarball/0.1',
+    download_url = 'https://github.com/mohan3d/PyOpenload/tarball/0.2',
     install_requires=[
         'requests',
     ],
     description='Just a python wrapper for openload.co API',
-    long_description=open('README.md').read(),
 )
