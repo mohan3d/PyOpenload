f65c5518332f8a66023ee4116ee97a2698abfacd
mohan3d <mohan3d94@gmail.com>
2017-08-09 23:38:38 +0200

    Add rtd theme to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import sphinx_rtd_theme
 #
 # PyOpenload documentation build configuration file, created by
 # sphinx-quickstart on Wed Aug  9 22:03:50 2017.
@@ -79,11 +81,12 @@ todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+# html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
