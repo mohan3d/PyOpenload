c48025f1b6da3f84ddba40cf5d4b5b8f67c8d611
mohan3d <mohan3d94@gmail.com>
2017-08-14 15:06:02 +0200

    Minor update in download_link example

--- examples/download_link.py
+++ examples/download_link.py
@@ -5,8 +5,8 @@ from openload import OpenLoad
 username = 'FTP Username/API Login'
 key = 'FTP Password/API Key'
 file_id = 'Id of the file will be downloaded'
-ticket = 'Ticket found in \'prepare_download\' response'
-captcha_response = 'Solution of captcha found in \'prepare_download\' response'
+ticket = 'Ticket found in `prepare_download` response'
+captcha_response = 'Solution of captcha found in `prepare_download` response'
 
 openload = OpenLoad(username, key)
 resp = openload.get_download_link(file_id,
