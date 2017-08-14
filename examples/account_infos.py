0c42e7e3dabe98cc26a73815b5dd590712cf85c1
mohan3d <mohan3d94@gmail.com>
2017-08-14 15:01:23 +0200

    Add download_link.py

--- examples/account_infos.py
+++ examples/account_infos.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 
 from openload import OpenLoad
 
-# Find them in https://openload.co/account#usersettings
 username = 'FTP Username/API Login'
 key = 'FTP Password/API Key'
 
---
+++ examples/download_link.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+from openload import OpenLoad
+
+username = 'FTP Username/API Login'
+key = 'FTP Password/API Key'
+file_id = 'Id of the file will be downloaded'
+ticket = 'Ticket found in \'prepare_download\' response'
+captcha_response = 'Solution of captcha found in \'prepare_download\' response'
+
+openload = OpenLoad(username, key)
+resp = openload.get_download_link(file_id,
+                                  ticket,
+                                  captcha_response)
+
+direct_download_url = resp.get('url')
+
+print(direct_download_url)
